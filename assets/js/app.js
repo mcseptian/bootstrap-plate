0549fc1b9a2cc0928e9e0e328aa032c76b3a17a4
Septian Adi <septian.adi@softwareseni.com>
2020-04-08 16:54:46 +0700

    chore(build): add scroll hidden on js.

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,10 +10,32 @@
 
         init: function($) {
             app.slide();
+            app.showButton();
         },
 
         onResize: function() {
             // call function here to applied on resize window
+        },
+
+        showButton: function() {
+            var button  = $('#top'), //button that scrolls user to top
+                view = $(window),
+                timeoutKey = -1;
+        
+            $(document).on('scroll', function() {
+                if(timeoutKey) {
+                    window.clearTimeout(timeoutKey);
+                }
+                timeoutKey = window.setTimeout(function(){
+        
+                    if (view.scrollTop() < 100) {
+                        button.fadeOut();
+                    }
+                    else {
+                        button.fadeIn();
+                    }
+                }, 100);
+            });
         },
 
         // ======================================================================
@@ -31,7 +53,7 @@
                     navText: ['<button class="btn icon--large rounded-circle bgcolor--yellow"><i class="fa fa-arrow-left text-dark"></i></button>','<button class="btn icon--large rounded-circle bgcolor--yellow"><i class="fa fa-arrow-right text-dark"></i></button>']
                 })
             });
-        },
+        }
     }
 
     $(document).ready(function () {
